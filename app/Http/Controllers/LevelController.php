be42a6deaec25b98beb22a0ea10649fac216f72a
Nasywa Syafinka Widyamara <syafinkaforcolle@gmail.com>
2024-09-14 19:21:33 +0700

    jobsheet 3-praktikum 6-Implementasi Eloquent ORM

--- app/Http/Controllers/LevelController.php
+++ app/Http/Controllers/LevelController.php
@@ -7,22 +7,21 @@ use Illuminate\Support\Facades\DB;
 
 class LevelController extends Controller
 {
-    public function index(){
-        // TAMBAH DATA
-        // DB::insert('insert into m_level(level_kode, level_nama, created_at) values(?,?,?)',['CUS', 'Pelanggan', now()]);
-        // return 'Insert data baru berhasil';
+    public function index() {
+        // Untuk menambahkan data
+        // DB::insert('insert into m_level(level_kode, level_nama, created_at) values(?, ?, ?)', ['CUS', 'Pelanggan', now()]);
+        // return 'Insert data baru berhasil'; 
 
-        // UPDATE DATA
+        // Mengupdate data
         // $row = DB::update('update m_level set level_nama = ? where level_kode = ?', ['Customer', 'CUS']);
-        // return 'Update data berhasil. Jumlah data yang diupdate: ' .$row. 'baris';
+        // return 'Update data berhasil. Jumlah data yang diupdate : ' . $row . ' baris';
 
-        // DELETE DATA
-        // $row=DB::delete('delete from m_level where level_kode = ?', ['CUS']);
-        // return 'Delete data berhasil. Jumlah data yang dihapus: '. $row . ' baris';
+        // Untuk menghapus data
+        // $row = DB::delete('delete from m_level where level_kode = ?', ['CUS']);
+        // return 'Delete data berhasil. Jumlah data yang dihapus : ' .$row. ' baris';
 
-        // VIEW
+        // Untuk menampilkan view
         $data = DB::select('select * from m_level');
-        return view('level', ['data' => $data]);
-        
+        return view('level_id', ['data' => $data]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,30 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\LevelController;
 use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\LevelController;
 use App\Http\Controllers\UserController;
-use App\Models\UserModel;
-use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route untuk LevelController
 Route::get('/level', [LevelController::class, 'index']);
+
+// Route untuk KategoriController
 Route::get('/kategori', [KategoriController::class, 'index']);
+
+// Route untuk UserController
+Route::get('/user', [UserController::class, 'index']);
