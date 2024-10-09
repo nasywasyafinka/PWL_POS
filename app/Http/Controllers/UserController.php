053f47f1e7e3bfd055da40ba6fa9dcdfbd7ffaa1
Nasywa Syafinka Widyamara <syafinkaforcolle@gmail.com>
2024-10-09 08:48:16 +0700

    Jobsheet 6-Praktikum 3 (Modal Ajax Hapus Data (Data User))

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -263,4 +263,31 @@ class UserController extends Controller
         }
         return redirect('/');
     }
+
+    // Menampilkan halaman confirm hapus
+    public function confirm_ajax(string $id) {
+        $user = UserModel::find($id);
+        return view('user.confirm_ajax', ['user' => $user]);
+    }
+
+    // Menghapus data user dengan AJAX
+    public function delete_ajax(Request $request, $id) {
+        //cek apakah request dari ajax
+        if($request->ajax() || $request->wantsJson()) {
+            $user = UserModel::find($id);
+            if($user) {
+                $user->delete();
+                return response()->json([
+                    'status' => true,
+                    'message' => 'Data berhasil dihapus'
+                ]);
+            } else {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Data tidak ditemukan'
+                ]);
+            }
+        }
+        return redirect('/');
+    }
 }
